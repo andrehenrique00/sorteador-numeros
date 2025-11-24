2103cc051d64c890ae0411d1218b9cc1e887104b
Andre Henrique <adr.h3r@gmail.com>
2025-11-24 18:13:15 -0300

    Adiciona validação para o intervalo de entrada na função sortear

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ function sortear(){
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
+    
+    if (de >= ate) {
+    alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+    return;
+    }
 
     let sorteados = [];
     let numero;
